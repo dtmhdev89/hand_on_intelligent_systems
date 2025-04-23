9d6b2fd54c3e38c195823061e5762ce41eadb8e2
Tran Minh Hieu Dinh <jrvn-hieu@Hieus-MacBook-Pro.local>
2025-04-23 20:44:23 +0700

    gemini image generation

--- simple_ai_text_image_prompt_app/providers/google/gemini_model.py
+++ simple_ai_text_image_prompt_app/providers/google/gemini_model.py
@@ -1,15 +1,20 @@
 from config.settings import env_settings
 from google import genai
+import base64
 
 
 class GeminiModel:
     def __init__(self) -> None:
-        self.model_name = "gemini-2.0-flash"
+        self.model_name_for_text = "gemini-2.0-flash"
+        self.model_name_for_image = "gemini-2.0-flash-exp-image-generation"
         self._client = None
-        self.generate_content_config = genai.types.GenerateContentConfig(
+        self.generate_text_content_config = genai.types.GenerateContentConfig(
             # system_instruction=
             # temperature=
             max_output_tokens=100
+        )
+        self.generate_image_config = genai.types.GenerateContentConfig(
+            response_modalities=['TEXT', 'IMAGE']
         )
 
     def client(self):
@@ -25,11 +30,42 @@ class GeminiModel:
 
         try:
             response = self.client().models.generate_content(
-                model=self.model_name,
+                model=self.model_name_for_text,
                 contents=[prompt],
-                config=self.generate_content_config
+                config=self.generate_text_content_config
             )
 
             return response.text
         except Exception as e:
-            return str(e)
+            print(str(e))
+            raise
+
+    def image_generation(self, prompt):
+        """Image generation"""
+
+        try:
+            response = self.client().models.generate_content(
+                model=self.model_name_for_image,
+                contents=[prompt],
+                config=self.generate_image_config
+            )
+
+            return GeminiModel.filter_out_images(response)
+        except Exception as e:
+            print(e)
+            raise
+
+    @staticmethod
+    def filter_out_images(response):
+        """Filter out images from response and encode them as base64"""
+
+        images = []
+
+        for part in response.candidates[0].content.parts:
+            if part.inline_data is not None:
+                base64_encoded = base64.b64encode(part.inline_data.data).\
+                                        decode('utf-8')
+                data_url = f"data:image/png;base64,{base64_encoded}"
+                images.append(data_url)
+        
+        return images
--- simple_ai_text_image_prompt_app/routers/gen_ai.py
+++ simple_ai_text_image_prompt_app/routers/gen_ai.py
@@ -3,7 +3,7 @@ from fastapi import APIRouter, status, \
     HTTPException
 from fastapi.responses import JSONResponse
 from providers.provider import AIProvider
-
+import base64
 
 router = APIRouter(
     prefix="/genai",
@@ -18,7 +18,8 @@ class GenAI:
     @router.get("/text/{provider}/{prompt}", status_code=status.HTTP_200_OK)
     def text_generation(provider: str, prompt: str):
         """Text Generation API through AI Provider"""
-        if not provider in AI_PROVIDERS:
+
+        if provider not in AI_PROVIDERS:
             raise HTTPException(
                 status_code=status.HTTP_400_BAD_REQUEST,
                 detail="Unsupported provider"
@@ -29,5 +30,30 @@ class GenAI:
 
         return JSONResponse(
             content={"answer": answer},
+            status_code=status.HTTP_200_OK
+        )
+    
+    @staticmethod
+    @router.get("/image/{provider}/{prompt}", status_code=status.HTTP_200_OK)
+    def image_generation(provider: str, prompt: str):
+        """Image Generation API through AI Provider"""
+
+        if provider not in AI_PROVIDERS:
+            raise HTTPException(
+                status_code=status.HTTP_400_BAD_REQUEST,
+                detail="Unsupported provider"
+            )
+        
+        genai_model = AIProvider(provider=provider).factory_model()
+        image_responses = genai_model.image_generation(prompt=prompt)
+
+        if not image_responses:
+            raise HTTPException(
+                status_code=status.HTTP_400_BAD_REQUEST,
+                detail="Cannot generated any image"
+            )
+        
+        return JSONResponse(
+            content={"images": image_responses},
             status_code=status.HTTP_200_OK
         )
